7d701b1b20161e8533a6da9ba60c5b7ea70e3755
Edison1223 <limjunwei1346@gmail.com>
2024-11-26 16:35:19 +0800

    This is my first Java code

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    
+    public static void main(String[] args) {
+        String name ="Lim";
+        int age =25;
+        double height = 1.80;
+        boolean Student =true;
+        long networth = 123000000L;
+        System.out.println("The name of person is "+name+" and his age is "+age);
+        System.out.println("The height of student is"+height+"and is he a student ? " + Student);
+        System.out.println("His networth is "+ networth);
+    }
+}
